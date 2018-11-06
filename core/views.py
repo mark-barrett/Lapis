973d7520ef726cd60953dadd435314df6a439455
Mark Barrett <mark.barrett3@mydit.ie>
2018-11-06 14:49:51 +0000

    Login
    - Added login authentication

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -6,6 +9,21 @@ class Home(View):
 
     def get(self, request):
         return render(request, 'core/home.html')
+
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Try log the user in
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            messages.success(request, 'Hey ' + user.first_name + ', welcome back!')
+            return redirect('/dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('/')
 
 
 class Dashboard(View):
